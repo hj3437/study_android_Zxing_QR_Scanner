04417bbec353643d024d990ba4a9b5d6a224f587
HyukJIn <insort@naver.com>
2021-08-17 23:34:37 +0900

    QR코드 스캔 기본사용방법

--- app/src/main/java/com/example/qr/MainActivity.kt
+++ app/src/main/java/com/example/qr/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.qr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.zxing.integration.android.IntentIntegrator
+import android.widget.Toast
+
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var qrcodeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        qrcodeButton = findViewById(R.id.qr_reader_button)
+        qrcodeButton.setOnClickListener {
+            // `this` is the current Activity
+            IntentIntegrator(this).initiateScan()
+        }
+    }
+
+    // Get the results:
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
